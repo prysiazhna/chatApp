56bdfd9d33ec02435b45c3b04fc53640b8d161ff
olhaprysiazhna <olha.prysiazhna@gmail.com>
2024-05-26 17:12:15 -0700

    Fixes

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,8 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:3000", {
-                    // const socket = io("https://chat-app-yt.onrender.com", {
+            // const socket = io("http://localhost:3000", {
+            const socket = io("https://chatapp-k09m.onrender.com/", {
                     query: {
                         userId: authUser._id,
                     },
