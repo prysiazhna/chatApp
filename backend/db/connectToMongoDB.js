11f0423ffc47ff68b7ceaffe1fbacaf916b84e2d
prysiazhna <43825375+prysiazhna@users.noreply.github.com>
2024-10-19 09:08:40 +0300

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoDB = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to db");
 
     } catch (error) {
