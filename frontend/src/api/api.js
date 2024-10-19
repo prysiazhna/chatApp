31805ff86b0bead10f71b583d45f16947d64aafe
prysiazhna <43825375+prysiazhna@users.noreply.github.com>
2024-10-19 08:12:17 +0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,11 @@
+const BASE_API_URL = "https://prysiazhna-chat-app-server.vercel.app";
+
 const API_URLS = {
-    LOGIN: "/api/auth/login",
-    LOGOUT: "/api/auth/logout",
-    SIGN_UP: "/api/auth/signup",
-    GET_MESSAGES: "/api/messages/",
-    SEND_MESSAGE: "/api/messages/send/",
-    UPLOAD_IMAGE: "/api/upload/",
-    GET_CONVERSATIONS: "/api/users"
+    LOGIN: `${BASE_API_URL}/api/auth/login`,
+    LOGOUT: `${BASE_API_URL}/api/auth/logout`,
+    SIGN_UP: `${BASE_API_URL}/api/auth/signup`,
+    GET_MESSAGES: `${BASE_API_URL}/api/messages/`,
+    SEND_MESSAGE: `${BASE_API_URL}/api/messages/send/`,
+    UPLOAD_IMAGE: `${BASE_API_URL}/api/upload/`,
+    GET_CONVERSATIONS: `${BASE_API_URL}/api/users`
 };
-export default API_URLS;
